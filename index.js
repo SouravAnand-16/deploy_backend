669d9e12f55dbc5f5538135b0450c04c7eed9722
Sourav Anand <sauravanand324@gmail.com>
2024-02-14 17:30:25 +0530

    cors

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT ;
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json(),cors());
 
 app.get("/",(req,res)=>{
     res.status(200).send({"msg":"Welcome to home page"});
